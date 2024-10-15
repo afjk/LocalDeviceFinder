0d5065383c9daae5c6e78c7595cfcacfef27da2d
afjk <afjk01@gmail.com>
2024-10-16 01:08:08 +0900

    Update

--- Assets/Sample-Dev/LocalDeviceFinderClient.cs
+++ Assets/Sample-Dev/LocalDeviceFinderClient.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class LocalDeviceFinderClient : MonoBehaviour
 {
-    [SerializeField] private int sndPort = 8081;
+    [SerializeField] private int sndPort = 8080;
     [SerializeField] private int rcvPort = 8080;
 
+    DeviceResponder responder;
     
     // Start is called before the first frame update
     void Start()
     {
+        responder = new DeviceResponder(new ReceiveDataFactory(), rcvPort, sndPort);
+        responder.StartListening();
     }
 }
