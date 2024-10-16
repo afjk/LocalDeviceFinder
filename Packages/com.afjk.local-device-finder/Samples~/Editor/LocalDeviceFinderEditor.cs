71a74680abdbc2abd518b4d030cf296a1f0c0644
afjk <afjk01@gmail.com>
2024-10-16 09:29:32 +0900

    Update Sample

--- Packages/com.afjk.local-device-finder/Samples~/Editor/LocalDeviceFinderEditor.cs
+++ Packages/com.afjk.local-device-finder/Samples~/Editor/LocalDeviceFinderEditor.cs
@@ -1,131 +1,178 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Timers;
 using UnityEditor;
 using UnityEngine;
 
-class DeviceData
+
+namespace com.afjk.LocalDeviceFinder.sample
 {
-    public DeviceData(string dataDeviceName, string ipAddress)
+    class DeviceData
     {
-        DeviceName = dataDeviceName;
-        IpAddress = ipAddress;
-    }
-
-    public string DeviceName { get; set; }
-    public string IpAddress { get; set; }
-}
-
-public class LocalDeviceFinderEditor : EditorWindow
-{
-    private int sndPort = 8080;
-    private int rcvPort = 8080;
-    private DeviceSearcher searcher;
-    private DeviceResponder responder;
-    private List<DeviceData> deviceList = new(); // List to hold the devices
-    private bool useMulticast = false;
-    private string multicastIP = "239.0.0.222"; // デフォルトのマルチキャストアドレス
-
-    [MenuItem("Tools/Local Device Finder")]
-    public static void ShowWindow()
-    {
-        GetWindow<LocalDeviceFinderEditor>("Local Device Finder");
-    }
-
-    private void OnGUI()
-    {
-        GUILayout.Label("Local Device Finder", EditorStyles.boldLabel);
-
-        // ポート設定
-        sndPort = EditorGUILayout.IntField("Send Port", sndPort);
-        rcvPort = sndPort;
-
-        bool newUseMulticast = EditorGUILayout.Toggle("Use Multicast", useMulticast);
-
-        if (newUseMulticast)
+        public DeviceData(string dataDeviceName, string ipAddress)
         {
-            multicastIP = EditorGUILayout.TextField("Multicast IP", multicastIP);
+            DeviceName = dataDeviceName;
+            IpAddress = ipAddress;
         }
 
-        // マルチキャスト設定が変更された場合、searcherとresponderを停止してnullにする
-        if (newUseMulticast != useMulticast)
+        public string DeviceName { get; set; }
+        public string IpAddress { get; set; }
+    }
+
+    public class LocalDeviceFinderEditor : EditorWindow
+    {
+        private int sndPort = 8080;
+        private int rcvPort = 8080;
+        private DeviceSearcher searcher;
+        private DeviceResponder responder;
+        private List<DeviceData> deviceList = new(); // List to hold the devices
+        private bool useMulticast = false;
+        private string multicastIP = "239.0.0.222"; // デフォルトのマルチキャストアドレス
+        Timer findTimer;
+        private string currentState = "";
+        private string receiveMessage = "";
+
+        [MenuItem("Tools/Local Device Finder")]
+        public static void ShowWindow()
         {
-            StopAll(); // 動作中のsearcherやresponderを停止
-            useMulticast = newUseMulticast;
+            GetWindow<LocalDeviceFinderEditor>("Local Device Finder");
         }
 
-        if (GUILayout.Button("Start Finding"))
+        private void OnGUI()
         {
-            deviceList.Clear();
-            if (searcher == null)
+            GUILayout.Label("Local Device Finder", EditorStyles.boldLabel);
+
+            // ポート設定
+            sndPort = EditorGUILayout.IntField("Send Port", sndPort);
+            rcvPort = sndPort;
+
+            bool newUseMulticast = EditorGUILayout.Toggle("Use Multicast", useMulticast);
+
+            if (newUseMulticast)
             {
-                searcher = new DeviceSearcher(new ReceiveDataFactory(), rcvPort, useMulticast ? multicastIP : null);
-                searcher.StartReceiving(OnReceiveDeviceData);
+                multicastIP = EditorGUILayout.TextField("Multicast IP", multicastIP);
             }
 
-            if (useMulticast)
+            if (newUseMulticast != useMulticast)
             {
-                searcher.SendMulticast(sndPort);
+                StopAll();
+                useMulticast = newUseMulticast;
+            }
+
+            if (GUILayout.Button("Start Finding"))
+            {
+                deviceList.Clear();
+                StopAll();
+                searcher = new DeviceSearcher(new ReceiveDataFactory(), rcvPort, useMulticast ? multicastIP : null);
+                searcher.StartReceiving(OnReceiveDeviceData);
+
+                if (useMulticast)
+                {
+                    searcher.SendMulticast(sndPort);
+                }
+                else
+                {
+                    searcher.SendBroadcast(sndPort);
+                }
+
+                Debug.Log("Finding started");
+
+                findTimer = new System.Timers.Timer(5000);
+                findTimer.Elapsed += (sender, e) =>
+                {
+                    searcher.StopReceiving();
+                    searcher = null;
+                    Debug.Log("Finding stopped");
+                };
+                findTimer.AutoReset = false;
+                findTimer.Start();
+
+                currentState = "StartFinding";
+            }
+
+            if (GUILayout.Button("Start Receiver"))
+            {
+                deviceList.Clear();
+                StopAll();
+                responder = new DeviceResponder(new ReceiveDataFactory(), rcvPort, sndPort,
+                    useMulticast ? multicastIP : null);
+                responder.StartListening(((bytes, ipAddress) =>
+                {
+                    var message = Encoding.Unicode.GetString(bytes);
+                    if (string.IsNullOrEmpty(message)) return;
+                    receiveMessage = $"{message} from {ipAddress}";
+                    Debug.Log(receiveMessage);
+                    UnityEditor.EditorApplication.delayCall += Repaint;
+                }));
+                Debug.Log("Receiver started");
+
+                currentState = "StartReceiver";
+            }
+
+            if (GUILayout.Button("Stop"))
+            {
+                StopAll();
+                deviceList.Clear();
+                Debug.Log("Receiver stopped");
+
+                currentState = "Stop";
+            }
+
+            // Display the list of devices or the receiving state based on the current state
+            if (currentState == "StartFinding")
+            {
+                GUILayout.Label("Say Hello to Discovered Devices:", EditorStyles.boldLabel);
+                foreach (var device in deviceList)
+                {
+                    if (GUILayout.Button($"Device: {device.DeviceName}, IP: {device.IpAddress}"))
+                    {
+                        byte[] message = Encoding.Unicode.GetBytes($"Hello");
+                        StopAll();
+                        searcher = new DeviceSearcher(new ReceiveDataFactory(), rcvPort,
+                            useMulticast ? multicastIP : null);
+                        searcher.SendUnicast(message, device.IpAddress, sndPort);
+                    }
+                }
+            }
+            else if (currentState == "StartReceiver")
+            {
+                GUILayout.Label("Receiving", EditorStyles.boldLabel);
+                if (!string.IsNullOrEmpty(receiveMessage))
+                {
+                    GUILayout.Label(receiveMessage, EditorStyles.boldLabel);
+                }
             }
             else
             {
-                searcher.SendBroadcast(sndPort);
+                GUILayout.Label("Waiting", EditorStyles.boldLabel);
+            }
+        }
+
+        private void OnReceiveDeviceData(IReceiveData idata, string ipAddress)
+        {
+            var data = idata as ReceiveData;
+            Debug.Log($"OnReceiveDeviceData: {data.DeviceName}");
+            // 既にリストに存在しない場合のみ追加
+            if (!deviceList.Any(d => d.IpAddress == ipAddress))
+            {
+                deviceList.Add(new DeviceData(data.DeviceName, ipAddress));
             }
 
-            Debug.Log("Finding started");
-
-            Timer timer = new System.Timers.Timer(5000);
-            timer.Elapsed += (sender, e) =>
-            {
-                searcher.StopReceiving();
-                searcher = null;
-                Debug.Log("Finding stopped");
-            }; // Stop receiving when the timer elapses
-            timer.AutoReset = false;
-            timer.Start();
+            UnityEditor.EditorApplication.delayCall += Repaint;
         }
 
-        if (GUILayout.Button("Start Receiver"))
+        private void StopAll()
         {
-            if (responder == null)
-            {
-                responder = new DeviceResponder(new ReceiveDataFactory(), rcvPort, sndPort, useMulticast ? multicastIP : null);
-            }
-            responder.StartListening();
-            Debug.Log("Receiver started");
-        }
+            findTimer?.Stop();
+            findTimer?.Dispose();
+            findTimer = null;
 
-        if (GUILayout.Button("Stop Receiver"))
-        {
-            StopAll();
-            Debug.Log("Receiver stopped");
+            searcher?.StopReceiving();
+            responder?.StopListening();
+            searcher = null;
+            responder = null;
+            receiveMessage = "";
         }
-
-        // Display the list of devices
-        GUILayout.Label("Discovered Devices:", EditorStyles.boldLabel);
-        foreach (var device in deviceList)
-        {
-            GUILayout.Label($"Device: {device.DeviceName}, IP: {device.IpAddress}");
-        }
-    }
-
-    private void OnReceiveDeviceData(IReceiveData idata, string ipAddress)
-    {
-        var data = idata as ReceiveData;
-        Debug.Log($"OnReceiveDeviceData: {data.DeviceName}");
-        // 既にリストに存在しない場合のみ追加
-        if (!deviceList.Any(d => d.IpAddress == ipAddress))
-        {
-            deviceList.Add(new DeviceData(data.DeviceName, ipAddress));
-        }
-        UnityEditor.EditorApplication.delayCall += Repaint;
-    }
-
-    private void StopAll()
-    {
-        searcher?.StopReceiving();
-        responder?.StopListening();
-        searcher = null;
-        responder = null;
     }
 }
--- Packages/com.afjk.local-device-finder/Samples~/ReceiveData.cs
+++ Packages/com.afjk.local-device-finder/Samples~/ReceiveData.cs
@@ -2,64 +2,68 @@ using System;
 using System.Text;
 using UnityEngine;
 
-public class ReceiveDataFactory : IReceiveDataFactory
+namespace com.afjk.LocalDeviceFinder.sample
 {
-    string deviceName;
-    public ReceiveDataFactory()
+    public class ReceiveDataFactory : IReceiveDataFactory
     {
-        deviceName = SystemInfo.deviceName;
+        string deviceName;
+
+        public ReceiveDataFactory()
+        {
+            deviceName = SystemInfo.deviceName;
+        }
+
+        public IReceiveData Create()
+        {
+            return new ReceiveData(deviceName);
+        }
     }
-    
-    public IReceiveData Create()
-    {
-        return new ReceiveData(deviceName);
-    }
-}
 
 
-/// <summary>
-/// Json format
-/// {"d": "DeviceName"}
-/// </summary>
-public class ReceiveData : IReceiveData
-{
-    public string DeviceName
+    /// <summary>
+    /// Json format
+    /// {"d": "DeviceName"}
+    /// </summary>
+    public class ReceiveData : IReceiveData
     {
-        get => d;
-        private set => d = value;
-    }
+        public string DeviceName
+        {
+            get => d;
+            private set => d = value;
+        }
 
-    public string d; // Device name. Serialize時のkey名を短くするために使用
+        public string d; // Device name. Serialize時のkey名を短くするために使用
 
-    public ReceiveData(string deviceName)
-    {
-        DeviceName = deviceName;
-    }
+        public ReceiveData(string deviceName)
+        {
+            DeviceName = deviceName;
+        }
 
-    public ReceiveData(byte[] bytes)
-    {
-        var data = Deserialize(bytes) as ReceiveData;
-        this.DeviceName = data.DeviceName;
-    }
-    
-    private string ToJson()
-    {
-        return JsonUtility.ToJson(this);
-    }
+        public ReceiveData(byte[] bytes)
+        {
+            var data = Deserialize(bytes) as ReceiveData;
+            this.DeviceName = data.DeviceName;
+        }
 
-    private static ReceiveData FromJson(string jsonString)
-    {
-        return JsonUtility.FromJson<ReceiveData>(jsonString);
-    }
-    
-    public byte[] Serialize()
-    {
-        return Encoding.Unicode.GetBytes(ToJson());
-    }
+        private string ToJson()
+        {
+            return JsonUtility.ToJson(this);
+        }
 
-    public IReceiveData Deserialize(byte[] bytes)
-    {
-        var message = Encoding.Unicode.GetString(bytes);
-        return FromJson(message);
+        private static ReceiveData FromJson(string jsonString)
+        {
+            return JsonUtility.FromJson<ReceiveData>(jsonString);
+        }
+
+        public byte[] Serialize()
+        {
+            return Encoding.Unicode.GetBytes(ToJson());
+        }
+
+        public IReceiveData Deserialize(byte[] bytes)
+        {
+            var message = Encoding.Unicode.GetString(bytes);
+            return FromJson(message);
+        }
     }
 }
